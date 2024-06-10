129c765bfd38e3d8a3373b465dc3d819c2433118
alfathroziqq <alfathroziq94@gmail.com>
2024-06-10 23:44:41 +0700

    login register frontend

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,12 @@ use App\Http\Controllers\AdministratorController;
 use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
 
-// Halaman Welcome
+// Halaman Login Register
 Route::get('/', function () {
-    return view('/loginregister/login');
-});
+    return view('loginregister.login');
+})->name('login');
+
+// Halaman Login Register
+// Route::get('/', function () {
+//     return view('loginregister.register');
+// })->name('register');
