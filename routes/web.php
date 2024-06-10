9eed9ff26d8d357f593655e8d001c3f3a6d4da07
alfathroziqq <alfathroziq94@gmail.com>
2024-06-11 00:23:46 +0700

    baruu

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,17 @@ use App\Http\Controllers\AdministratorController;
 use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
 
-// Halaman Login
+// Halaman Home View
 Route::get('/', function () {
+    return view('home.home_view');
+})->name('home');
+
+// Halaman Login
+Route::get('/login', function () {
     return view('loginregister.login');
 })->name('login');
 
 // Halaman Register
-// Route::get('/', function () {
-//     return view('loginregister.register');
-// })->name('register');
-
-// Halaman Home Views
-Route::get('/', function () {
-    return view('home.homeViews');
-})->name('homeViews');
+Route::get('/register', function () {
+    return view('loginregister.register');
+})->name('register');
