b84c64a71c25210bf53321f120476057d9e72a74
Adidya Abimanyu <adidyaabimanyu07@gmail.com>
2024-06-11 00:17:45 +0700

    Nama

--- resources/views/home/home_administrator.blade.php
+++ resources/views/home/home_administrator.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Manusia Pintar 2024</title>
+    <title>Administrator</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@500&display=swap" rel="stylesheet">
     <style>
