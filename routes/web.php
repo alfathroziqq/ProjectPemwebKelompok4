8e3238fca904856dfaecb65f15bed5c562516f6b
alfathroziqq <alfathroziq94@gmail.com>
2024-06-10 23:57:38 +0700

    front end baru

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,17 @@ use App\Http\Controllers\AdministratorController;
 use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
 
-// Halaman Login Register
+// Halaman Login
 Route::get('/', function () {
     return view('loginregister.login');
 })->name('login');
 
-// Halaman Login Register
+// Halaman Register
 // Route::get('/', function () {
 //     return view('loginregister.register');
 // })->name('register');
+
+// Halaman Home Views
+Route::get('/', function () {
+    return view('home.homeViews');
+})->name('homeViews');
