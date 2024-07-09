b7057576f95ee1fd7d66acb11752e8d2221ad2d2
alfathroziqq <alfathroziq94@gmail.com>
2024-07-09 08:53:16 +0700

    FIX PROJECT

--- resources/views/admin_wilayah/home_admin_wilayah.blade.php
+++ resources/views/admin_wilayah/home_admin_wilayah.blade.php
@@ -271,7 +271,7 @@
             const dataSpek = {
                 labels: ['Rumah Sehat', 'Rumah Tidak Sehat', 'Rumah Tidak Layak'],
                 datasets: [{
-                    label: 'Jumlah Rumah Sehat',
+                    label: 'Spesifikasi Rumah',
                     backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)'],
                     borderColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)'],
                     data: [countRumahSehat, countRumahTidakSehat,
